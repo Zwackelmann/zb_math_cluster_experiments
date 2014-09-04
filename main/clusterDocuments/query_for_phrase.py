e66d7670976608bbc74de0533ea528022b8771f8
Simon Barthel <simon@welt-erobern.de>
2014-09-04 14:06:17 +0200

    1

--- main/clusterDocuments/query_for_phrase.py
+++ main/clusterDocuments/query_for_phrase.py
@@ -14,7 +14,7 @@ tdm = load_csr_matrix("derived_data/zb_math_full_text_tdm2.npz")
 translateMap = json.load(open("derived_data/zb_math_full_texts_tokens2IndexMap"))
 row_number2fulltext_id_map = json.load(open("derived_data/row_number2fulltext_id_map.json"))
 
-phrase = "our task is now to"
+phrase = "theorem"
 tokenizer = DocumentParser.TextTokenizer()
 tokens = tokenizer.tokenize(phrase)
 tokenIds = map(lambda token: translateMap[token], tokens)
@@ -33,9 +33,43 @@ for i in range(len(m.indptr)-1):
 		candidateInd.append(currInd)
 	currInd += 1
 
+print str(len(candidateInd)) + " Candidates\n"
+
 candidateIds = map(lambda i : row_number2fulltext_id_map[str(i)], candidateInd)
 candidateDocumentFilenames = map(lambda id : filter(lambda c : c in ascii_letters or c in digits, id) + ".xml.npy", candidateIds) 
 candidateDocumentFilepaths = map(lambda filename : join("derived_data/full_text_arrays", filename), candidateDocumentFilenames)
 
+def hasTokenSequence(sent, tokens):
+	matches = 0
+	for token in sent:
+		if token == tokens[matches]:
+			matches += 1
+		else:
+			matches = 0
+		
+		if matches == len(tokens):
+			return True
+
+	return False
+
 for filepath in candidateDocumentFilepaths:
-	print np.load(filepath)
+	arr = np.load(filepath)
+	
+	for par in arr:
+		for sent in par:
+			if hasTokenSequence(sent, tokens):
+				print repr(par) + "\n\n"
+				break
+				
+
+
+
+
+
+
+
+
+
+
+
+
